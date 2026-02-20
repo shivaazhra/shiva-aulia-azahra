34b86cc9773f8d9b435f4b738a903a60c5b72c29
shivaazhra <shivaaulia0309@gmail.com>
2026-02-20 15:09:16 +0700

    Add files via upload

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    protected $primaryKey = 'nim';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'matakuliah'
+    ];
+}
